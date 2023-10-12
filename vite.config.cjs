75c1386df7df021220686a860e93781776f1a430
dskelton-cos <dskelton@cityofsydney.nsw.gov.au>
2023-10-13 09:03:07 +1100

    adding vite handlebars

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+
   server: {
     open: "/index.html",
   },
@@ -8,4 +11,9 @@ export default defineConfig({
     outDir: "../dist",
   },
   root: "src",
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/partials'),
+    }),
+  ],
 });
