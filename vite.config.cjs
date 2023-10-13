dc77f69a0686bdae2c46b6f8bac83c4b52502340
dskelton-cos <dskelton@cityofsydney.nsw.gov.au>
2023-10-13 13:03:22 +1100

    adding values

--- vite.config.cjs
+++ vite.config.cjs
@@ -2,18 +2,40 @@ import { defineConfig } from "vite";
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
-export default defineConfig({
+function handlebarsOverride(options) {
+  const plugin = handlebars(options);
+  // Currently handleHotUpdate skips further processing, which bypasses
+  // postcss and in turn tailwind doesn't pick up file changes
+  delete plugin.handleHotUpdate;
+  return plugin;
+}
 
-  server: {
-    open: "/index.html",
-  },
+export default defineConfig({
+  root: "src",
   build: {
     outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        components: resolve(__dirname, 'src/components.html'),
+      },
+    },
   },
-  root: "src",
   plugins: [
-    handlebars({
+    handlebarsOverride({
       partialDirectory: resolve(__dirname, 'src/partials'),
+      helpers: {
+        json: (context) => {
+          return JSON.stringify(context);
+        }
+      }
     }),
   ],
+  server: {
+    open: "/index.html",
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+  },
 });
